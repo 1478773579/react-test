664160c15c93ade2c093a0519a5867aeba1ef064
chenxueping <chenxueping@sudiyi.cn>
2020-04-28 23:24:01 +0800

    Updates

---
+++ precache-manifest.6b8e1ed46a62ee02ea9d54f7836cb23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d47f215d6aa23198b299f87999980b",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "5bbcbb7ae637afadaba7",
+    "url": "/react-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d9de7524c633167b5b9",
+    "url": "/react-test/static/js/2.296acbdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test/static/js/2.296acbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbcbb7ae637afadaba7",
+    "url": "/react-test/static/js/main.66417685.chunk.js"
+  },
+  {
+    "revision": "4cc28344971920d237ea",
+    "url": "/react-test/static/js/runtime-main.228dbcd0.js"
+  }
+]);
